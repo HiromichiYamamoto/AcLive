7fa2cf0fc92d158c703f95463c956d456b6185a3
Hiromichi Yamamoto <hiromichi.yamamoto@ari-jp.com>
2018-06-19 08:49:19 +0900

    add profile

---
+++ resources/views/ac/profile/input.blade.php
@@ -0,0 +1,11 @@
+@extends('ac.layouts.base')
+
+@section('content')
+    <section class="section-1">
+        <div class="wrapper">
+            <div class="ProInputMain">
+                <div class="Title">プロフィール編集</div>
+            </div>
+        </div>
+    </section>
+@endsection
